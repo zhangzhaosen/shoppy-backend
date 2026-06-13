43704aec7263e440d37a59f3759abbd053270bf2
zhangzhaosen <zhangzhaosen@sina.com>
2026-06-13 09:06:39 +0800

    feat: prisma

---
+++ src/users/dto/create-user.request.ts
@@ -0,0 +1,10 @@
+
+import { IsEmail, IsStrongPassword } from 'class-validator';
+export class CreateUserRequest{
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword()
+  password: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request';
+
+@Injectable()
+export class UsersService {
+  createUser(data: CreateUserRequest) {
+
+    console.log(data)
+  }
+}
